d7cbb7748b6dd6961abd515bc80ba71e254bb634
Dominic Myers <annoyingmouse@gmail.com>
2015-07-01 18:08:46 +0100

    A couple of fixes for version 3 of the APK
    Downloading it caused some issues so I guess the new SDK isn't backwards compatible. :-(

--- createtables.php
+++ createtables.php
@@ -1,11 +1,19 @@
 <?php
 //Code is provided by AWS and available here; http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/AppendixSampleDataCodePHP.html
 
+// Date now needs to be set, which I guess is a good thing!
+date_default_timezone_set('Europe/London');
+// Find out what the issues are:
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
 require '/var/www/html/vendor/autoload.php';
 use Aws\DynamoDb\DynamoDbClient;
 
 $client = DynamoDbClient::factory(array(
-    'region' => 'eu-west-1'  // replace with your desired region
+    'region' => 'eu-west-1',  // replace with your desired region
+    'version' => '2012-08-10' // Now need a version
 ));
 
 $tableNames = array();
@@ -146,7 +154,7 @@ $tableNames[] = $tableName;
 
 foreach($tableNames as $tableName) {
     echo "Waiting for table $tableName to be created." . PHP_EOL;
-    $client->waitUntilTableExists(array('TableName' => $tableName));
+    $client->waitUntilTableExists(array('TableName' => $tableName)); // this is causing issues - will investigate further
     echo "Table $tableName has been created." . PHP_EOL;
 }
 
